8be7add774fa6c118cf5caf8389f3d5645e2d4b6
KRNagarjun <kr.nagarjun@ltts.com>
2021-04-27 13:25:54 +0530

    Nuget Library

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Humanizer;
+
+namespace NugetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Car".Pluralize());
+            Console.WriteLine("Pant".Pluralize());
+            Console.WriteLine("Octopus".Pluralize());
+
+            Console.WriteLine(3500.ToWords());
+            Console.WriteLine(400.Billions());
+            Console.WriteLine("25-Mar-2021".Titleize());
+        }
+    }
+}
